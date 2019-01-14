df136d8cdf45a1ab36c768c1890532f4dac6ac29
Ferdy Halim <ferdyhalimansyah27@gmail.com>
2019-01-14 09:56:47 +0700

    add transition splash screen

--- app/src/main/java/com/example/hazelnut/dummy/SplashActivity.java
+++ app/src/main/java/com/example/hazelnut/dummy/SplashActivity.java
@@ -5,13 +5,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+
 
         new Handler().postDelayed(new Runnable(){
             @Override
@@ -19,6 +24,7 @@ public class SplashActivity extends Activity {
                 /* Create an Intent that will start the Menu-Activity. */
                 Intent mainIntent = new Intent(SplashActivity.this,LoginActivity.class);
                 SplashActivity.this.startActivity(mainIntent);
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
                 SplashActivity.this.finish();
             }
         }, 1000);
