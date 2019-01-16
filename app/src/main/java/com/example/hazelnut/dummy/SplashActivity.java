4b9b5d9113cd5d7625fa60c58fa0327691feee5a
Ferdy Halim <ferdyhalimansyah27@gmail.com>
2019-01-16 11:41:35 +0700

    splash scre, logo glow

--- app/src/main/java/com/example/hazelnut/dummy/SplashActivity.java
+++ app/src/main/java/com/example/hazelnut/dummy/SplashActivity.java
@@ -1,21 +1,38 @@
 package com.example.hazelnut.dummy;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.util.DisplayMetrics;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import static java.security.AccessController.getContext;
 
 public class SplashActivity extends Activity {
+
+    ImageView splash;
+    Context context = this;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+
+        splash = findViewById(R.id.splash);
+
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        final int screenh = displayMetrics.heightPixels;
+        final int screenw = displayMetrics.widthPixels;
+
 
 
         new Handler().postDelayed(new Runnable(){
@@ -26,6 +43,9 @@ public class SplashActivity extends Activity {
                 SplashActivity.this.startActivity(mainIntent);
                 overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
                 SplashActivity.this.finish();
+
+                splash.setMaxWidth(screenh);
+                splash.setMaxWidth(screenw);
             }
         }, 1000);
     }
